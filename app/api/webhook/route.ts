2ec39359e928b7f5d953640fdf955496d68aa00c
Joell Alex <joellalex922@gmail.com>
2026-02-27 13:19:11 +0530

    added

--- app/api/webhook/route.ts
+++ app/api/webhook/route.ts
@@ -1,5 +1,6 @@
 import { NextResponse } from "next/server";
-
+import { getInstallationOctokit } from "@/lib/github/installations";
+import { addComment } from "@/lib/github/comments";
 
 export async function POST(request: Request) {
   const body = await request.json();
@@ -8,8 +9,27 @@ export async function POST(request: Request) {
   console.log("Action:", body.action);
   console.log("Repo:", body.repository?.full_name);
 
+
+  // reqd var we will extract from the webhook payload for demonstration
+  const owner = body.repository?.owner?.login;
+  const repo = body.repository?.name;
+  const prNumber = body.pull_request?.number;
   const pr = body.pull_request;
 
+    const octokit = await getInstallationOctokit(
+      Number(process.env.INSTALLATION_ID)
+    );
+  
+    await addComment({
+      octokit,
+      owner: owner,
+      repo: repo,
+      issue_number: prNumber,
+      body: "👋 PR received automatically by GitHub App!",
+    });
+  
+    console.log("Comment posted!");
+  
   if (pr && pr.patch_url) {
     console.log("Fetching patch from:", pr.patch_url);
 
---
+++ lib/github/octokit.ts
@@ -0,0 +1,14 @@
+import fs from "fs";
+import { App } from "octokit";
+import dotenv from "dotenv";
+
+dotenv.config();
+const keyPath = process.env.GITHUB_PRIVATE_KEY!;
+const privateKey = fs.readFileSync(keyPath, "utf8");
+
+const app = new App({
+  appId: process.env.GITHUB_APP_ID!,
+  privateKey,
+});
+
+export default app;
