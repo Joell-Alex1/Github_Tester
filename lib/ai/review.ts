491da767d2d2a4b79fa26c699f2b59f88882dd33
Joell Alex <joellalex922@gmail.com>
2026-03-07 21:05:43 +0530

    changes ai model hope it works

--- lib/ai/review.ts
+++ lib/ai/review.ts
@@ -6,7 +6,7 @@ const ai = new GoogleGenAI({apiKey: process.env.GEMINI_API_KEY ? process.env.GEM
 export async function reviewCode(fileContent: string): Promise<string> {
   try {
     const response = await ai.models.generateContent({
-      model: "gemini-1.5-flash",
+      model: "gemini-2.5-flash",
       contents: fileContent,
       config: {
         systemInstruction: `You are a senior software engineer reviewing a pull request.
